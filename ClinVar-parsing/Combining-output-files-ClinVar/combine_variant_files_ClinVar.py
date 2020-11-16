76e8bf363d61503130c054db4287f81559fd4d0c
David Sant <david.sant@utah.edu>
2020-11-16 10:12:04 -0700

    Updated ClinVar combine_variant_files_ClinVar.py to include gnomAD frequency information and new titles of columns

--- ClinVar-parsing/Combining-output-files-ClinVar/combine_variant_files_ClinVar.py
+++ ClinVar-parsing/Combining-output-files-ClinVar/combine_variant_files_ClinVar.py
@@ -3,24 +3,32 @@ import json5
 import os
 import glob
 import pandas as pd
+import copy
 
 
-args = ArgumentParser('./combine_variant_files_ClinVar.py', description='This program is a simple one to combine the output files of each group of variants (invalid HGVS, no star variants, variants with stars) into a single csv file. Example usage: ./combine_variant_files_ClinVar.py --ClinVar_directory output_files --disease_names SCID Metabolic_Diseases --output_directory output_files')
+args = ArgumentParser('''./combine_variant_files_ClinVar.py', description='This program is a simple one to combine the output
+	files of each group of variants (invalid HGVS, no star variants, variants with stars) into a single csv file. Example usage:
+	./combine_variant_files_ClinVar.py --ClinVar_directory output_files --disease_names SCID Metabolic_Diseases --output_directory output_files''')
 
 args.add_argument(
+	'-c',
 	'--ClinVar_directory',
-	help="This is the name of the directory where the variants from the ClinVar parser are stored. The final output file will be stored in this directory if no output directory is stored with the option --output_directory. Default is output_files.",
+	help="""This is the name of the directory where the variants from the ClinVar parser are stored. The final output file will be stored in this
+	directory if no output directory is stored with the option --output_directory. Default is output_files.""",
 	default = "output_files"
 )
 
 args.add_argument(
+	'-d',
 	'--disease_names',
 	nargs='+',
-	help="This is the same list of disease names supplied into ClinVar_Parser.py. If this is not specified, the program will try to gather the information from the directory specified with --ClinVar_directory. ",
+	help="""This is the same list of disease names supplied into ClinVar_Parser.py. If this is not specified, the program will try to gather
+	the information from the directory specified with --ClinVar_directory. """,
 	default = None
 )
 
 args.add_argument(
+	'-o',
 	'--output_directory',
 	help="This is where your output file will be stored. If this option is not used, the directory specified in --ClinVar_directory will be used.",
 	default = None
@@ -42,15 +50,19 @@ if not disease_names:
 	for subdir in subdirectories:
 		disease_names.append(subdir.rsplit('/')[-2])
 
-columns_Invalid = ['Genome Assembly', 'Chr', 'Position Start', 'Position Stop', 'Ref', 'Alt',
+columns_valid = ['Genome Assembly', 'Chr', 'Position Start', 'Position Stop', 'Ref', 'Alt',
 			'Genomic Annotation', 'HGVS Normalized Genomic Annotation', 'Variant Type',
 			'Variant Length', 'Pathogenicity', 'Disease', 'Genetic Origin', 'Inheritance Pattern',
 			'Affected Genes', 'Gene Symbol', 'dbSNP ID', 'Compound Het Status', 'Transcript',
 			'Transcript Notation', 'HGVS Transcript Notation', 'Protein Accession',
 			'Protein Notation', 'HGVS Protein Annotation', 'Chr Accession', 'VCF Pos', 'VCF Ref',
-			'VCF Alt', 'Database', 'ClinVar Accession', 'Review Status', 'Star Level',
-			'Submitter', 'Edited Date', 'Transcript Normalization Failure Message',
-			'Genomic Normalization Failure Message', 'HGVS Normalization Failure Reason']
+			'VCF Alt', 'Database', 'Database Accession', 'Review Status', 'Star Level',
+			'Submitter', 'Edited Date', 'Overall_MAF', 'Control_MAF', 'African_MAF',
+			'NonFinish_Euro_MAF', 'Finnish_MAF', 'Ashkenazi_MAF','Latino_MAF', 'East_Asian_MAF',
+			'South_Asian_MAF', 'Other_Ancestry_MAF', 'Transcript Normalization Failure Message',
+			'Genomic Normalization Failure Message']
+columns_Invalid = copy.deepcopy(columns_valid)
+columns_Invalid.append('HGVS Normalization Failure Reason')
 
 merged_invalid = pd.DataFrame(columns = columns_Invalid)
 
@@ -61,16 +73,6 @@ for disease_name in disease_names:
 		tmp_df = pd.read_csv(file, index_col = 0)
 		merged_invalid = pd.concat([merged_invalid, tmp_df]).reset_index(drop=True)
 merged_invalid.to_csv(output_directory+"/ClinVar_All_Invalid_HGVS_Annotations.csv")
-
-columns_valid = ['Genome Assembly', 'Chr', 'Position Start', 'Position Stop', 'Ref', 'Alt',
-			'Genomic Annotation', 'HGVS Normalized Genomic Annotation', 'Variant Type',
-			'Variant Length', 'Pathogenicity', 'Disease', 'Genetic Origin', 'Inheritance Pattern',
-			'Affected Genes', 'Gene Symbol', 'dbSNP ID', 'Compound Het Status', 'Transcript',
-			'Transcript Notation', 'HGVS Transcript Notation', 'Protein Accession',
-			'Protein Notation', 'HGVS Protein Annotation', 'Chr Accession', 'VCF Pos', 'VCF Ref',
-			'VCF Alt', 'Database', 'ClinVar Accession', 'Review Status', 'Star Level',
-			'Submitter', 'Edited Date', 'Transcript Normalization Failure Message',
-			'Genomic Normalization Failure Message']
 
 merged_stars = pd.DataFrame(columns = columns_valid)
 
