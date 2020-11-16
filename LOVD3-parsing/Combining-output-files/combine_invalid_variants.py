af94856aa716b64fd54afa69a9ecd0cd7465d3a0
David Sant <david.sant@utah.edu>
2020-11-16 08:34:17 -0700

    Updated LOVD3 combine_invalid_variants.py to include Database Accession and gnomAD frequency columns

--- LOVD3-parsing/Combining-output-files/combine_invalid_variants.py
+++ LOVD3-parsing/Combining-output-files/combine_invalid_variants.py
@@ -5,29 +5,39 @@ import glob
 import pandas as pd
 
 
-args = ArgumentParser('./combine_invalid_variants.py', description='This program is a simple one to combine the output files containing the invalid variants (anything that failed HGVS normalization) into one csv file. Example usage: ./combine_invalid_variants.py --config_file LOVD3_Databases.json --LOVD3_directory output_files --disease_names SCID Metabolic_Diseases --output_directory output_files')
+args = ArgumentParser('./combine_invalid_variants.py', description='''This program is a simple one to combine the output files containing the
+invalid variants (anything that failed HGVS normalization) into one csv file. Example usage:
+./combine_invalid_variants.py --config_file LOVD3_Databases.json --LOVD3_directory output_files
+--disease_names SCID Metabolic_Diseases --output_directory output_files''')
 
 args.add_argument(
+	'-c',
 	'--config_file',
 	#type=FileType('r'),
-	help="This is the config file that was used for the LOVD3 parser. If this option is not provided, the program will look for a json file in your present working directory.",
+	help="""This is the config file that was used for the LOVD3 parser. If this option is not provided,
+	the program will look for a json file in your present working directory.""",
 	default = None,
 )
 
 args.add_argument(
+	'-l',
 	'--LOVD3_directory',
-	help="This is the name of the directory where the variants from LOVD3 parser are stored. The final output file will be stored in this directory if no output directory is stored with the option --output_directory. Default is output_files.",
+	help="""This is the name of the directory where the variants from LOVD3 parser are store d. The final output file will be stored
+	in this directory if no output directory is stored with the option --output_directory. Default is output_files.""",
 	default = "output_files"
 )
 
 args.add_argument(
+	'-d',
 	'--disease_names',
 	nargs='+',
-	help="This is the same list of disease names supplied into LOVD3_Variant_Parser.py. If this is not specified, the program will try to gather the information from the directory specified with --LOVD3_directory. ",
+	help="""This is the same list of disease names supplied into LOVD3_Variant_Parser.py. If this is not specified, the program will
+	try to gather the information from the directory specified with --LOVD3_directory.""",
 	default = None
 )
 
 args.add_argument(
+	'-o',
 	'--output_directory',
 	help="This is where your output file will be stored. If this option is not used, the directory specified in --LOVD3_directory will be used.",
 	default = None
@@ -74,11 +84,13 @@ columns = ['Genome Assembly', 'Chr', 'Position Start', 'Position Stop', 'Ref', '
 			'Affected Genes', 'Gene Symbol', 'Compound Het Status', 'Transcript',
 			'Transcript Notation', 'HGVS Transcript Notation', 'Protein Accession',
 			'Protein Notation', 'HGVS Protein Annotation', 'Chr Accession', 'VCF Pos', 'VCF Ref',
-			'VCF Alt', 'Database', 'ClinVar Accession', 'Review Status', 'Star Level',
+			'VCF Alt', 'Database', 'Database Accession', 'Review Status', 'Star Level',
 			'Submitter', 'Edited Date', 'DNA change (genomic) (hg19)', 'Effect',
 			'Exon','Reported', 'DB-ID', 'dbSNP ID', 'Published as', 'Variant remarks',
-			'Reference', 'Frequency', 'Transcript Normalization Failure Message',
-			'Genomic Normalization Failure Message', 'HGVS Normalization Failure Reason']
+			'Reference', 'Frequency', 'ClinVar Accession','Transcript Normalization Failure Message',
+			'Genomic Normalization Failure Message', 'Overall_MAF', 'Control_MAF', 'African_MAF',
+			'NonFinish_Euro_MAF', 'Finnish_MAF', 'Ashkenazi_MAF', 'Latino_MAF', 'East_Asian_MAF',
+			'South_Asian_MAF', 'Other_Ancestry_MAF', 'HGVS Normalization Failure Reason']
 
 merged_df = pd.DataFrame(columns = columns)
 for database in databases_list:
